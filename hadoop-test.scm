e01d3114fe1dafd6a5b0f9ef5eb1dc43c3162e79
Peter Danenberg <pcd@roxygen.org>
2013-04-14 22:45:57 -0700

    Inchoate

--- hadoop-test.scm
+++ hadoop-test.scm
@@ -2,7 +2,7 @@
 
 ;; [[file:~/prg/scm/hadoop/TODO.org::*Compiling][Compiling:5]]
 
-(use shell)
+(use debug expand-full shell)
 
 (define-syntax hadoop
   (syntax-rules ()
@@ -10,5 +10,15 @@
      (run (hadoop exp ...)))))
 
 (hadoop fs -ls /)
+
+(define-syntax (stream-hadoop kvlist)
+  (kvlist-reduce (lambda (key value args)
+                   (cons* key value args))
+                 '()
+                 kvlist))
+
+(define-syntax hadoop-streaming
+  (lambda (expression rename compare)
+    `(stream-hadoop ,(list `expression))))
 
 ;; Compiling:5 ends here
